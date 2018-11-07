e7f725d2590160ab96fa755a57059931161380c7
MariaJMRibeiro <44523769+MariaJMRibeiro@users.noreply.github.com>
2018-11-07 10:24:19 +0000

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,23 +1,24 @@
 CREATE TABLE user (
     username VARCHAR PRIMARY KEY,
     password VARCHAR NOT NULL,
-    email VARCHAR,
-    fullname VARCHAR,
-    profile_pic varBinary(MAX)
+    email VARCHAR NOT NULL,
+    fullname VARCHAR NOT NULL,
+    profile_pic varBinary(MAX),
+    score INTEGER NOT NULL,
 );
 
 CREATE TABLE post (
     id INTEGER PRIMARY KEY,
-    username VARCHAR REFERENCES user,
-    title VARCHAR,
-    fulltext VARCHAR
-    post_votes INTEGER,
+    title VARCHAR NOT NULL,
+    fulltext VARCHAR NOT NULL,
+    post_votes INTEGER NOT NULL,
+    username VARCHAR NOT NULL REFERENCES user,
 );
 
 CREATE TABLE comments (
     id INTEGER PRIMARY KEY,
-    post_id INTEGER REFERENCES post,
-    username VARCHAR REFERENCES user,
-    text VARCHAR,
-    comments_votes INTEGER,
+    post_id INTEGER NOT NULL REFERENCES post,
+    username VARCHAR NOT NULL REFERENCES user,
+    text VARCHAR NOT NULL,
+    comments_votes INTEGER NOT NULL,
 );
