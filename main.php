8775ba4c5e2ccc755d3acc0e120302d106affca5
Patrícia Rocha <rochapatricia20@gmail.com>
2018-11-13 19:33:31 +0000

    Update main page

--- main.php
+++ main.php
@@ -1,33 +1,41 @@
 <?php include_once('database/connection.php');
 
 # Query returning all news in the database:
-$stmt = $db->prepare('SELECT * FROM post');
-$stmt->execute();
-$posts = $stmt->fetch(); ?>
+$query = $db->prepare('SELECT * FROM post');
+$query->execute();
+$posts = $query->fetch(); ?>
 
 <!DOCTYPE html>
 <html lang="en-US">
 
   <head>
-      <meta name="viewport" content="width=device-width, initial-scale=1">
-      <link href="style.css" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="style.css" rel="stylesheet">
   </head>
 
   <body>
+
     <header id="topBar">
-        <h2>Reddit</h2>
-        <button id="logInBtn" class="button"> Log In </button>
-
-        <div id="logIn" class="logInWindow">
-            <div class="logInForm">
-                <span class="closeBtn"> &times; </span>
-                <h3>Log In</h3>
-                <form>
-
-                </form>
-            </div>
+      <h2>Reddit</h2>
+      <button id="logInBtn" class="button"> Log In </button>
+      <div id="logIn" class="logInWindow">
+        <div id="logInForm">
+          <span class="closeBtn"> &times; </span>
+          <h3>Login</h3>
+          <form>
+            <label>Username: 
+              <input type="text" placeholder="Enter username" name="username" required>
+            </label>
+            <br><br>
+            <label>Password:
+              <input type="password" placeholder="Enter password" name="password" required>
+            </label>
+            <br><br>
+            <button formaction="action_login.php" formmethod="post">Login</button>
+          </form>
         </div>
-        <script src="main.js"></script>
+      </div>
+      <script src="main.js"></script>
     </header>
 
     <section id="post">
@@ -35,17 +43,21 @@ $posts = $stmt->fetch(); ?>
       foreach($posts as $post) {
       ?>
       <article>
-        <?php echo '<h1>' . $post['title'] . '</h1>';
-        echo '<p>' . $article['fulltext'] . '</p>'; ?>
+        <?php 
+        echo '<h3>' . $post['title'] . '</h3>';
+        echo '<p>' . $post['fulltext'] . '</p>'; 
+        ?>
         <footer>
           <span id="username"><?php echo '<p> Posted by' . $post['username'] . '</p>';?></span>
         </footer>
       </article>
+      <?php } ?>
     </section>
+
   </body>
 
   <footer>
     <p>&copy; Reddit, 2018</p>
   </footer>
-
+  
 </html>
