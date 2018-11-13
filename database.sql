fb40a47f56809a67c0593095db874f0969933c8c
Patrícia Rocha <rochapatricia20@gmail.com>
2018-11-13 14:00:20 +0000

    Final database script

--- database.sql
+++ database.sql
@@ -1,28 +1,28 @@
 CREATE TABLE user (
     username VARCHAR PRIMARY KEY,
-    password NVARCHAR320 NOT NULL,
-    email VARCHAR NOT NULL,
-    fullname VARCHAR NOT NULL,
-    profile_pic varbinary(MAX),
-    score INTEGER NOT NULL,
+    password BINARY(32) NOT NULL,
+    email VARCHAR(320) NOT NULL, -- 64 characters for local part + @ + 255 for domain name
+    fullname VARCHAR(128) NOT NULL,
+    profile_pic VARBINARY,
+    score INTEGER NOT NULL DEFAULT 0 CHECK (score >= 0)
 );
 
 CREATE TABLE post (
-    id INTEGER PRIMARY KEY,
-    username VARCHAR NOT NULL REFERENCES user,
+    id SERIAL PRIMARY KEY,
+    username VARCHAR REFERENCES user NOT NULL,
     title VARCHAR NOT NULL,
     fulltext VARCHAR NOT NULL,
-    post_score INTEGER NOT NULL,
+    post_score INTEGER NOT NULL DEFAULT 0 CHECK (post_score >= 0)
 );
 
-CREATE TABLE comments (
-    id INTEGER PRIMARY KEY,
-    post_id INTEGER NOT NULL REFERENCES post,
-    username VARCHAR NOT NULL REFERENCES user,
+CREATE TABLE comment (
+    id SERIAL PRIMARY KEY,
+    post_id SERIAL REFERENCES post NOT NULL,
+    username VARCHAR REFERENCES user NOT NULL,
     text VARCHAR NOT NULL,
-    comments_score INTEGER NOT NULL,
+    comment_score INTEGER NOT NULL DEFAULT 0 CHECK (comment_score >= 0)
 );
 
-INSERT INTO user VALUES ('maria','201506196','mariajmribeiro@hotmail.com', 'Maria Ribeiro', null, 0);
-INSERT INTO user VALUES ('vw','201504834','bemlindo@hotmail.com', 'João Pereira', null, 0);
-INSERT INTO user VALUES ('tixa','201504616','rochapatricia@gmail.com', 'Patrícia Rocha', null, 0);
+INSERT INTO user (username, password, email, fullname) VALUES ('maria','201506196','mariaribeiro@gmail.com', 'Maria Ribeiro');
+INSERT INTO user (username, password, email, fullname) VALUES ('vw','201504834','joaopereira@gmail.com', 'João Pereira');
+INSERT INTO user (username, password, email, fullname) VALUES ('tixa','201504616','patriciarocha@gmail.com', 'Patrícia Rocha');
