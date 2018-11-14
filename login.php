c9ad7b6993ce2b909231e1cd2381f8aae49d775d
Patrícia Rocha <rochapatricia20@gmail.com>
2018-11-14 16:49:24 +0000

    Created login page

---
+++ login.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en-US">
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="style.css" rel="stylesheet">
+</head>
+
+<body>
+    <form action="action_login.php" method="post">
+        <div class="imgcontainer">
+            <img src="" alt="Avatar" class="avatar">
+        </div>
+
+        <div class="container">
+            <label>Username:
+                <input type="text" placeholder="Enter username" name="uname" required>
+            </label>
+            <br><br>
+
+            <label>Password:
+                <input type="password" placeholder="Enter password" name="psw" required>
+            </label>
+            <br><br>
+
+            <button type="submit">Login</button>
+        </div>
+    </form>
+</body>
--- main.php
+++ main.php
@@ -1,33 +1,23 @@
 <?php include_once('database/connection.php');
 
 # Query returning all news in the database:
-$stmt = $db->prepare('SELECT * FROM post');
-$stmt->execute();
-$posts = $stmt->fetch(); ?>
+$query = $db->prepare('SELECT * FROM post');
+$query->execute();
+$posts = $query->fetch(); ?>
 
 <!DOCTYPE html>
 <html lang="en-US">
 
   <head>
-      <meta name="viewport" content="width=device-width, initial-scale=1">
-      <link href="style.css" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="style.css" rel="stylesheet">
   </head>
 
   <body>
+
     <header id="topBar">
-        <h2>Reddit</h2>
-        <button id="logInBtn" class="button"> Log In </button>
-
-        <div id="logIn" class="logInWindow">
-            <div class="logInForm">
-                <span class="closeBtn"> &times; </span>
-                <h3>Log In</h3>
-                <form>
-
-                </form>
-            </div>
-        </div>
-        <script src="main.js"></script>
+      <h2>Reddit</h2>
+      <a href="login.php">Login</a>
     </header>
 
     <section id="post">
@@ -35,17 +25,21 @@ $posts = $stmt->fetch(); ?>
       foreach($posts as $post) {
       ?>
       <article>
-        <?php echo '<h1>' . $post['title'] . '</h1>';
-        echo '<p>' . $article['fulltext'] . '</p>'; ?>
+        <?php 
+        echo '<h3>' . $post['title'] . '</h3>';
+        echo '<p>' . $post['fulltext'] . '</p>'; 
+        ?>
         <footer>
           <span id="username"><?php echo '<p> Posted by' . $post['username'] . '</p>';?></span>
         </footer>
       </article>
+      <?php } ?>
     </section>
+
   </body>
 
   <footer>
-    <p>&copy; Reddit, 2018</p>
+    <p>&copy; 2018 Reddit</p>
   </footer>
-
+  
 </html>
