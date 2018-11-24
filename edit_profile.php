235ee8de1a4d10f580dcb3afa9618e2cb338efec
Patrícia Rocha <rochapatricia20@gmail.com>
2018-11-24 23:34:07 +0000

    css :lipstick:

--- edit_profile.php
+++ edit_profile.php
@@ -22,21 +22,28 @@
 
     <?php include_once('header.php'); ?>
 
+    <div class="title_container">
+        <h1><span class="edit-profile-title">Customize profile</span></h1>
+    </div>
+
+    <div class="image_container">
+        <form class="edit_image" action="action_upload.php" method="post" enctype="multipart/form-data">
+        <h3>Profile pic</h3>
+        <label>Select image to upload:</label>
+            <input type="file" name="image">
+            <input type="submit" value="Upload">
+        </form>
+    </div>
+
     <div class="edit_profile">
         <form class="edit_content" action="action_edit.php" method="post">
-            <div class="title_container">
-                <h1><span class="edit-profile-title">Customize profile</span></h1>
-            </div>
-            <hr>
             <h3>Profile information</h3>
 
             <div class="input_container">
-                <!--<label>Display name (optional)</label>-->
                 <input type="text" placeholder="Change/Add name" name="fullname">
             </div>
 
             <div class="input_container">
-                <!--<label>Change email</label>-->
                 <input type="text" placeholder="Change email" name="email">
             </div>
 
@@ -53,16 +60,6 @@
                 <button type="submit">Save changes</button>
             </div>
         </form>
-    </div>
-
-    <div class="input_container">
-
-        <p>Profile Pic: </p>
-        <form action="action_upload.php" method="post" enctype="multipart/form-data"> Select image to upload:
-            <input type="file" name="image">
-            <input type="submit" value="Upload">
-        </form>
-
     </div>
 
     <?php include_once('footer.php'); ?>
